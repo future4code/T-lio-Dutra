2caa8668df952c5765639e6d0916cc8b7aa38c2a
Venus <tmdiasdutra@icloud.com>
2021-06-14 11:10:53 -0300

    exercicio LabeX

---
+++ semana10/andromeda/andromeda/src/pages/HomePage/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import { ButtonsContainer } from "./styles";
+import { Link } from "react-router-dom";
+import PageTitle from "../../components/PageTitle";
+
+const HomePage = () => {
+    return <div>
+        <PageTitle title={"LabeX"}/>
+        <ButtonsContainer>
+            <Link to={"/login"}>
+                <Button variant={"outlined"} color={"secondary"}>Área do Administrador</Button>
+            </Link>
+            <Link to={"/inscricao"}>
+                <Button variant={"contained"} color={"primary"}>Quero me Candidatar!</Button>
+            </Link>
+        </ButtonsContainer>
+    </div>
+}
+
+export default HomePage
